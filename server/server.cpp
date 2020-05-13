0e3932387634134fdc90c65fcb62a362194eccdf
Georgy Khalin <halin.george@gmail.com>
2020-05-13 22:32:58 +0300

    Add abseil

--- server/server.cpp
+++ server/server.cpp
@@ -1,10 +1,13 @@
 #include "server/server.h"
 
+#include "glog/logging.h"
+
 #include <iostream>
 
 namespace tgnews {
 
 Server::Server(uint32_t port) : port_(port) {
+  LOG(INFO) << "Server listening on: " << port;
 }
 
 }  // namespace tgnews
