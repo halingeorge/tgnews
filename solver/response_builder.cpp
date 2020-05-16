0fac77f2a05c9091e3963eac3862e5270ba14bce
muradmazitov <blindedbymath@gmail.com>
2020-05-16 23:06:05 +0000

    add json to ans

--- solver/response_builder.cpp
+++ solver/response_builder.cpp
@@ -4,8 +4,8 @@ namespace tgnews {
 
   ResponseBuilder::ResponseBuilder(tgnews::Context* context) : Context(context) {}
 
-  CalculatedResponses ResponseBuilder::AddDocument(std::vector<tgnews::Document> /*docs*/) {
-    
+  CalculatedResponses ResponseBuilder::AddDocuments(const std::vector<tgnews::Document*>& docs) {
+    std::copy(docs.begin(), docs.end(), std::back_inserter(Docs));
   }
 
 }
--- solver/response_builder.h
+++ solver/response_builder.h
@@ -1,17 +1,22 @@
-#include "context.h"
+#include "server/context.h"
+#include "base/document.h"
+
+#include "json/json.h"
 
 namespace tgnews {
 
   class CalculatedResponses {
   public:
-    uint32_t tmp;
+    Json::Value GetAns(const std::string& lang, const std::string& category, const uint64_t period);
   };
 
   class ResponseBuilder {
   public:
     ResponseBuilder(tgnews::Context* context);
-    CalculatedResponses AddDocuments(const std::vector<tgnews::Document>& docs);
+    CalculatedResponses AddDocuments(const std::vector<tgnews::Document*>& docs);
   private:
+
+    std::vector<tgnews::Document*> Docs;
     tgnews::Context* Context;
   };
 
