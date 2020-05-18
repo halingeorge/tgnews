d8469ac39ba422382cf9c15bb0ef039c1124077d
muradmazitov <blindedbymath@gmail.com>
2020-05-18 21:22:44 +0000

    add correct news and lnaguages cli mode

--- solver/response_builder.cpp
+++ solver/response_builder.cpp
@@ -5,6 +5,8 @@
 
 namespace {
 
+using namespace tgnews;
+
 Json::Value CalcLangAns(const std::vector<tgnews::ParsedDoc>& docs) {
   Json::Value result;
   int idx = 0;
@@ -22,12 +24,81 @@ Json::Value CalcLangAns(const std::vector<tgnews::ParsedDoc>& docs) {
   return result;
 }
 
+Json::Value CalcNewsAns(const std::vector<tgnews::ParsedDoc>& docs) {
+  Json::Value articles;  
+  for (const auto& doc : docs) {
+    if (doc.Category != ENewsCategory::NC_NOT_NEWS && doc.Category != ENewsCategory::NC_UNDEFINED && doc.Category != ENewsCategory::NC_OTHER) {
+      articles.append(doc.FileName);
+    }
+  }
+  Json::Value result;
+  result["articles"] = std::move(articles);
+  return result;
+}
+
+Json::Value CalcCategoryAns(const std::vector<tgnews::ParsedDoc>& docs) {
+  Json::Value society;
+  for (const auto& doc : docs) {
+    if (doc.Category == ENewsCategory::NC_SOCIETY) {
+      society.append(doc.FileName);
+    }
+  }
+  Json::Value economy;
+  for (const auto& doc : docs) {
+    if (doc.Category == ENewsCategory::NC_ECONOMY) {
+      economy.append(doc.FileName);
+    }
+  }
+  Json::Value technology;
+  for (const auto& doc : docs) {
+    if (doc.Category == ENewsCategory::NC_TECHNOLOGY) {
+      technology.append(doc.FileName);
+    }
+  }
+  Json::Value sports;
+  for (const auto& doc : docs) {
+    if (doc.Category == ENewsCategory::NC_SPORTS) {
+      sports.append(doc.FileName);
+    }
+  }
+  Json::Value entertainment;
+  for (const auto& doc : docs) {
+    if (doc.Category == ENewsCategory::NC_ENTERTAINMENT) {
+      entertainment.append(doc.FileName);
+    }
+  }
+  Json::Value science;
+  for (const auto& doc : docs) {
+    if (doc.Category == ENewsCategory::NC_SCIENCE) {
+      science.append(doc.FileName);
+    }
+  }
+  Json::Value other;
+  for (const auto& doc : docs) {
+    if (doc.Category == ENewsCategory::NC_OTHER) {
+      other.append(doc.FileName);
+    }
+  }
+  Json::Value result;
+#define ADD_TO_RESULT(s) result[#s] = std::move(s);
+  ADD_TO_RESULT(society);
+  ADD_TO_RESULT(economy);
+  ADD_TO_RESULT(technology);
+  ADD_TO_RESULT(sports);
+  ADD_TO_RESULT(entertainment);
+  ADD_TO_RESULT(science);
+  ADD_TO_RESULT(other);
+  return result;
+}
+
 }
 
 namespace tgnews {
 
 CalculatedResponses::CalculatedResponses(const std::vector<tgnews::ParsedDoc>& docs) {
   LangAns = CalcLangAns(docs);
+  NewsAns = CalcNewsAns(docs);
+  CategoryAns = CalcCategoryAns(docs);
 }
 
 Json::Value CalculatedResponses::GetAns(const std::string& lang, const std::string& category, const uint64_t period) {
@@ -43,10 +114,8 @@ CalculatedResponses ResponseBuilder::AddDocuments(const std::vector<DocumentCons
 
   for (auto& doc : Docs) {
     doc.ParseLang(Context.LangDetect.get());
-  }
-
-  for (auto& doc : Docs) {
     doc.Tokenize(Context);
+    doc.DetectCategory(Context);
   }
 
   return {Docs};
