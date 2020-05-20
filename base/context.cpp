22419d1558e5083753b5ecf757a7e428cc1a0440
muradmazitov <blindedbymath@gmail.com>
2020-05-20 01:37:38 +0000

    add loading embedders

--- base/context.cpp
+++ base/context.cpp
@@ -1,4 +1,51 @@
 #include "base/context.h"
+
+#include <fstream>
+
+namespace {
+
+void LoadMatrixFromFile(Eigen::MatrixXf& matrix, std::string file) {
+  std::ifstream matrixIn(file);
+  std::string line;
+  int row = 0;
+  int col = 0;
+  while (std::getline(matrixIn, line)) {
+    std::string num;
+    for (char ch : line) {
+      if (ch == ' ') {
+        continue;
+        }
+      if (ch != ',' && ch != '\n') {
+        num += ch;
+        continue;
+      }
+      matrix(col++, row) = std::stof(num);
+      num = "";
+    }
+    if (!num.empty()) {
+      matrix(col, row) = std::stof(num);
+      num = "";
+    }
+    col = 0;
+    row++;
+  }
+  matrixIn.close();
+  assert(row != 0);
+  assert(row == 50);
+}
+
+void LoadBiasFromFile(Eigen::VectorXf& vec, std::string file) {
+  std::ifstream biasIn(file);
+  int row = 0;
+  std::string line;
+  while (std::getline(biasIn, line)) {
+    vec(row++) = std::stof(line);
+  }
+  biasIn.close();
+}
+
+}
+
 namespace tgnews {
 
 Context::Context(const std::string modelPath, std::unique_ptr<FileCache> fileCache)
@@ -11,6 +58,22 @@ Context::Context(const std::string modelPath, std::unique_ptr<FileCache> fileCac
   RuCatModel->loadModel(modelPath + "/ru_cat_v2.ftz");
   EnCatModel = std::make_unique<fasttext::FastText>();
   EnCatModel->loadModel(modelPath + "/en_cat_v2.ftz");
+  RuVecModel = std::make_unique<fasttext::FastText>();
+  RuVecModel->loadModel(modelPath + "/ru_vectors_v2.bin");
+  EnVecModel = std::make_unique<fasttext::FastText>();
+  EnVecModel->loadModel(modelPath + "/en_vectors_v2.bin");
+
+  RuMatrix = Eigen::MatrixXf(RuVecModel->getDimension() * 3, 50); 
+  LoadMatrixFromFile(RuMatrix, modelPath + "/ru_sentence_embedder/matrix.txt");
+
+  EnMatrix = Eigen::MatrixXf(EnVecModel->getDimension() * 3, 50);
+  LoadMatrixFromFile(EnMatrix, modelPath + "/en_sentence_embedder/matrix.txt");
+  
+  RuBias = Eigen::VectorXf(50);
+  LoadBiasFromFile(RuBias, modelPath + "/ru_sentence_embedder/bias.txt");
+
+  EnBias = Eigen::VectorXf(50);
+  LoadBiasFromFile(EnBias, modelPath + "/en_sentence_embedder/bias.txt");
 }
 
 }
--- base/context.h
+++ base/context.h
@@ -6,6 +6,7 @@
 
 #include "third_party/fastText/src/fasttext.h"
 #include "third_party/onmt_tokenizer/include/onmt/Tokenizer.h"
+#include "third_party/eigen/Eigen/Core"
 
 namespace tgnews {
 
@@ -16,6 +17,12 @@ class Context {
   std::unique_ptr<fasttext::FastText> LangDetect;
   std::unique_ptr<fasttext::FastText> RuCatModel;
   std::unique_ptr<fasttext::FastText> EnCatModel;
+  std::unique_ptr<fasttext::FastText> RuVecModel;
+  std::unique_ptr<fasttext::FastText> EnVecModel;
+  Eigen::MatrixXf RuMatrix;
+  Eigen::VectorXf RuBias;
+  Eigen::MatrixXf EnMatrix;
+  Eigen::VectorXf EnBias;
   onmt::Tokenizer Tokenizer;
 };
 
