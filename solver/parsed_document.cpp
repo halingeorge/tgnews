920d308d55e56b89df2e380a679e38f4fc002fb6
muradmazitov <blindedbymath@gmail.com>
2020-05-22 23:01:53 +0000

    date parsing

--- solver/parsed_document.cpp
+++ solver/parsed_document.cpp
@@ -5,9 +5,31 @@
 #include <third_party/tinyxml2/tinyxml2.h>
 
 #include <stdexcept>
+#include <ctime>
+#include <regex>
 
-static uint64_t DateToTimestamp(const std::string& d) {
-  return 0; // TODO(makeshit)
+static uint64_t DateToTimestamp(const std::string& date) {
+  std::regex ex("(\\d\\d\\d\\d)-(\\d\\d)-(\\d\\d)T(\\d\\d):(\\d\\d):(\\d\\d)([+-])(\\d\\d):(\\d\\d)");
+    std::smatch what;
+    if (!std::regex_match(date, what, ex) || what.size() < 10) {
+        throw std::runtime_error("wrong date format");
+    }
+    std::tm t = {};
+    t.tm_sec = std::stoi(what[6]);
+    t.tm_min = std::stoi(what[5]);
+    t.tm_hour = std::stoi(what[4]);
+    t.tm_mday = std::stoi(what[3]);
+    t.tm_mon = std::stoi(what[2]) - 1;
+    t.tm_year = std::stoi(what[1]) - 1900;
+
+    time_t timestamp = timegm(&t);
+    uint64_t zone_ts = std::stoi(what[8]) * 60 * 60 + std::stoi(what[9]) * 60;
+    if (what[7] == "+") {
+        timestamp = timestamp - zone_ts;
+    } else if (what[7] == "-") {
+        timestamp = timestamp + zone_ts;
+    }
+    return timestamp > 0 ? timestamp : 0;
 }
 
 static std::string GetFullText(const tinyxml2::XMLElement* element) {
